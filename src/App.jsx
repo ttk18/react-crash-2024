d1df25e31eb5af9091d246d0ef9362ac7955ad91
ttk18 <truongkiet1822002@gmail.com>
2024-05-15 18:42:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from 'react-router-dom'
+
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import JobsPage from './pages/JobsPage'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={<MainLayout />}
+        >
+            <Route
+                index
+                element={<HomePage />}
+            />
+            <Route
+                path="/jobs"
+                element={<JobsPage />}
+            />
+        </Route>
+    )
+)
+
+const App = () => {
+    return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import HomeCard from '../components/HomeCard'
+import JobListings from '../components/JobListings'
+import ViewAllJobs from '../components/ViewAllJobs'
+const HomePage = () => {
+    return (
+        <>
+            <Hero />
+            <HomeCard />
+            <JobListings />
+            <ViewAllJobs />
+        </>
+    )
+}
+
+export default HomePage
